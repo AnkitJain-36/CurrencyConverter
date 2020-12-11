56a5cff33ffe46497e0ea8b10574fd28c139786a
91965 <ankitjain@hcl.com>
2020-12-11 14:37:42 +0530

    Updated test case

--- GTest/GTest/TJsonCurrencyParser.cpp
+++ GTest/GTest/TJsonCurrencyParser.cpp
@@ -2,25 +2,12 @@
 #include "../../CJsonCurrencyParser.h"
 
 TEST(getCurrencyConversionFactor, noInputDefined) {
-	CJsonCurrencyParser parser("EUR");
+	CJsonCurrencyParser parser;
 	EXPECT_EQ(parser.getCurrencyConversionFactor("USD"), 0.0);
 }
 
 TEST(getCurrencyConversionFactor, inputDefined) {
-	CJsonCurrencyParser parser("EUR", "{\
-		\"success\":true,\
-		\"timestamp\" : 1607599745,\
-		\"base\" : \"EUR\",\
-		\"date\" : \"2020-12-10\",\
-		\"rates\" : {\
-		\"USD\":1.209183\
-	}\
-	}");
-	EXPECT_EQ(parser.getCurrencyConversionFactor("USD"), 1.209183);
-}
-
-TEST(getCurrencyConversionFactor, inputDefinedWithDelay) {
-	CJsonCurrencyParser parser("EUR");
+	CJsonCurrencyParser parser;
 	parser.setInput("{\
 		\"success\":true,\
 		\"timestamp\" : 1607599745,\
