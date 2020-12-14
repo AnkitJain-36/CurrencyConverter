1b21d17da8297dea3968db95b2f578f1272c403d
91965 <ankitjain@hcl.com>
2020-12-14 16:47:15 +0530

    Included check for currency code

--- main.cpp
+++ main.cpp
@@ -18,16 +18,24 @@ int main()
 
         // Get values
         std::cout << "Enter Values For Currency Conversion: \n";
+        std::cout << "Amount:";
+        std::cin >> amount;
         std::cout << "From:";
         //std::cin >> fromCurrencyCode;
         std::cout << "EUR" << std::endl;
         std::cout << "To:";
         std::cin >> toCurrencyCode;
-        std::cout << "Amount:";
-        std::cin >> amount;
 
         // Convert and Display Values
-        std::cout << "Result:" << CCurrencyConverter::convert(fromCurrencyCode, toCurrencyCode, amount) << std::endl;
+        auto convertedValue = CCurrencyConverter::convert(fromCurrencyCode, toCurrencyCode, amount);
+        if (convertedValue == 0.0)
+        {
+            std::cout << "Error: Country Code not found." << std::endl;
+        }
+        else
+        {
+            std::cout << "Result:" << convertedValue << std::endl;
+        }
 
         std::string cont;
         std::cout << "Countinue (y/n):";
